b1a8a1d5acc26044afb9bde7ad17a947a35670f5
soham19477 <55681105+soham19477@users.noreply.github.com>
2020-10-28 13:19:57 +0530

    Update Q1_part1.c
    Added code

--- Q1/Q1_part1.c
+++ Q1/Q1_part1.c
@@ -1,2 +1,31 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* Name: Soham Das
+   Roll_Number: 2019477 */
+#include <stdio.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <fcntl.h>
+#include <sys/errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+int x=10;
+
+int main()
+{
+    pid_t p;
+    p=fork();
+    if(p==-1){
+        printf("Error in fork\n");
+    }
+    if(p==0){
+        while(x>-90){
+            x--;
+            printf("\nChild: %d\n",x);
+        }
+    }
+    else{
+        while(x<100){
+            x++;
+            printf("\nParent: %d\n",x);
+        }
+    }
+}
