1389d0a9779c7ba81180021de3c2b9842386914b
soham19477 <55681105+soham19477@users.noreply.github.com>
2020-10-28 13:29:36 +0530

    Update test.c
    Added code

--- Q2/test.c
+++ Q2/test.c
@@ -1,2 +1,12 @@
-/* Name: Your Name
-   Roll_Number: Your Roll_Number */
+/* Name: Soham Das
+   Roll_Number: 2019477 */
+#include <stdio.h>
+#include <linux/kernel.h>
+#include <sys/syscall.h>
+#include <unistd.h>
+int main()
+{
+	 long int amma = syscall(440,getpid(),"a.txt");
+	 printf("System call sys_sh_task_info returned %ld\n", amma);
+	 return 0;
+}
